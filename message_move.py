080f7f790077bfe2a8470ac7a0ce7c99b563a9dc
small.dan <dan.small@clickbank.com>
2015-12-18 16:36:45 -0700

    refactored to be less n00b

--- message_move.py
+++ message_move.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# TODO: account for fewer messages than specified in arg so script doesn't hang open waiting for messages
+#!/usr/bin/python
 
 __author__ = "Dan Small"
 __version__ = "1.0.0"
@@ -9,35 +8,66 @@ __license__ = "GPL"
 
 import sys
 import optparse
+import json
+import stompest.error
 from stompest.config import StompConfig
 from stompest.protocol import StompSpec
 from stompest.sync import Stomp
 
 cliargs = optparse.OptionParser()
-cliargs.add_option('-b', '--broker', dest='broker', help='Hostname of ActiveMQ broker')
-cliargs.add_option('-f', '--from-queue', dest='from_queue', help='Queue name to move messages out of')
-cliargs.add_option('-t', '--to-queue', dest='to_queue', help='Queue name to move message to')
-cliargs.add_option('-n', '--num-messages', dest='num_messages', help='Number of messages to move')
+cliargs.add_option('-b', '--broker', dest='broker', default='localhost', help='Hostname of ActiveMQ broker')
+cliargs.add_option('-p', '--port', dest='port', default='61614', help='STOMP port on the broker')
+cliargs.add_option('-f', '--from-queue', dest='from_queue', default='test.alpha',
+                   help='Queue name to move messages out of')
+cliargs.add_option('-t', '--to-queue', dest='to_queue', default='test.beta', help='Queue name to move message to')
+cliargs.add_option('-c', '--close', dest='tmout', default=5,
+                   help='Amount of time in seconds to wait for a message before exiting')
 
 (options, args) = cliargs.parse_args()
 
 broker = options.broker
+port = options.port
 from_queue = options.from_queue
 to_queue = options.to_queue
-num_messages = options.num_messages
-
-config = StompConfig('tcp://{0}:61613'.format(broker))
+tmout = int(options.tmout)
+config = StompConfig('tcp://{0}:{1}'.format(broker, port))
 client = Stomp(config)
-client.connect()
+
+try:
+    client.connect()
+except stompest.error.StompConnectTimeout:
+    print('Unable to connect to {0}:{1}'.format(broker, port))
+    client.disconnect()
+    sys.exit(1)
+else:
+    print('STOMP connection established to {0}:{1}'.format(broker, port))
+
 client.subscribe(from_queue, {StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL})
 
-for i in range(num_messages):
+while client.canRead(timeout=tmout):
     frame = client.receiveFrame()
-    print 'body: {0}'.format(frame.body)
-    print 'headers: {0}'.format(frame.headers)
-    print 'Sending message to {0}'.format(to_queue)
-    client.ack(frame)
-    client.send(to_queue, body=frame.body, headers=frame.headers)
+    try:
+        payload = json.loads(frame.body)
+    except json.JSONDecoder:
+        print("Failed to decode message body: frame.body was {}".format(frame.body))
+        with open('/opt/apache-activemq/sendfail/malformed', 'a+') as mf:
+            mf.write('{0}\n{1}'.format(frame.headers, frame.body))
+            mf.close()
+        continue
+    headers = frame.headers
+    print('Sending the following message to {} : '.format(to_queue))
+    print('body: {}'.format(payload))
+    print('headers: {}'.format(frame.headers))
+    try:
+        client.ack(frame)
+    except stompest.error.StompConnectionError:
+        print('Unable to send ACK to {}'.format(broker))
+    try:
+        headers['persistent'] = 'true'
+        client.send(to_queue, body=json.dumps(payload), headers=headers)
+    except stompest.error.StompError:
+        print('Sending message to {} failed. Writing message locally at /opt/apache-activemq/sendfail'.format(broker))
+        with open('/opt/apache-activemq/sendfail/{}\n'.format(from_queue), 'a+') as sf:
+            sf.write(json.dumps(payload))
 
 client.disconnect()
-sys.exit()
